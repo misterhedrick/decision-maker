e02820fc041d03358a56851d8825f1c08a5d2bcc
Daniel Hedrick <misterhedrick@gmail.com>
2019-03-04 10:02:21 -0500

    firebase initialized

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,35 +1,37 @@
 import { NgModule, NO_ERRORS_SCHEMA } from "@angular/core";
 import { NativeScriptModule } from "nativescript-angular/nativescript.module";
-
+import { NativeScriptHttpModule } from "nativescript-angular/http";
+import { NativeScriptRouterModule } from "nativescript-angular/router";
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
+import { BackendService, FirebaseService, UtilsService } from "./services";
 import { ItemsComponent } from "./item/items.component";
 import { ItemDetailComponent } from "./item/item-detail.component";
-import firebase = require('nativescript-plugin-firebase');
 
 // Uncomment and add to NgModule imports if you need to use two-way binding
 // import { NativeScriptFormsModule } from "nativescript-angular/forms";
 
 // Uncomment and add to NgModule imports if you need to use the HttpClient wrapper
 // import { NativeScriptHttpClientModule } from "nativescript-angular/http-client";
-firebase
-    .init({})
-    .then(() => console.log('Firebase initialised!'))
-    .catch(() => console.error('Error!'));
 @NgModule({
     bootstrap: [
         AppComponent
     ],
     imports: [
         NativeScriptModule,
-        AppRoutingModule
+        AppRoutingModule,
+        NativeScriptHttpModule,
     ],
     declarations: [
         AppComponent,
         ItemsComponent,
         ItemDetailComponent
     ],
-    providers: [],
+    providers: [
+        BackendService,
+        FirebaseService,
+        UtilsService
+    ],
     schemas: [
         NO_ERRORS_SCHEMA
     ]
