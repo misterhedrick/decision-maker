0668e16a542d91027773f30e2443ab965a07dbc6
Daniel Hedrick <misterhedrick@gmail.com>
2019-03-04 11:47:05 -0500

    starting upload

--- src/app/item/items.component.ts
+++ src/app/item/items.component.ts
@@ -13,6 +13,13 @@ import { RouterExtensions } from 'nativescript-angular/router/router-extensions'
 })
 export class ItemsComponent implements OnInit {
     items: Item[];
+    id: string;
+    name: string;
+    description: string;
+    imagepath: string;
+    image: any;
+    private imagePath: string;
+    private uploadedImageName: string;
 
     // This pattern makes use of Angular’s dependency injection implementation to inject an instance of the ItemService service into this class.
     // Angular knows about this service because it is included in your app’s main NgModule, defined in app.module.ts.
@@ -25,5 +32,17 @@ export class ItemsComponent implements OnInit {
     logout() {
         this.firebaseService.logout();
         this.routerExtensions.navigate(["/login"], { clearHistory: true });
+    }
+    uploadImage() {
+        this.id = "id";
+        this.imagePath = "../src/app/assets/camera.png";
+        this.uploadedImageName = "camera";
+        this.description = "camera image";
+        this.firebaseService.uploadFile(this.imagePath).then((uploadedFile: any) => {
+            this.uploadedImageName = uploadedFile.name;
+            //get downloadURL and store it as a full path;
+        }, (error: any) => {
+            alert('File upload error: ' + error);
+        });
     }
 }
